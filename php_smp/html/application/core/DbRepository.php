b4213fa2af05efbce9c87cc58a2412d00cf15a52
shotaro tanaka <5511068t@gmail.com>
2021-01-23 14:44:28 +0900

    20210123

---
+++ php_smp/html/application/core/DbRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class DbRepository
+{
+    protected $con;
+
+    public function __construct($con) {
+        $this->setConnection($con);
+    }
+
+    public function setConnection($con)
+    {
+        $this->con = $con;
+    }
+}
+
+?>
