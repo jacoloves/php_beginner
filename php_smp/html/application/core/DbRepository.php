9c4300c439fbae0005246e04829d97a12b1c6f2d
shotaro tanaka <5511068t@gmail.com>
2021-01-24 15:21:55 +0900

    20210124

--- php_smp/html/application/core/DbRepository.php
+++ php_smp/html/application/core/DbRepository.php
@@ -12,6 +12,24 @@ abstract class DbRepository
     {
         $this->con = $con;
     }
+
+    public function execute($sql, $params = array())
+    {
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    public function fetch($sql, $params = array())
+    {
+        return $this->execute($sql, $params)->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function fetchAll($sql, $params = array())
+    {
+        return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
