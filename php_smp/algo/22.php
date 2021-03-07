165918a5d0f4264d5f431c7d1fc5abe7e3143c64
shotaro tanaka <5511068t@gmail.com>
2021-03-07 15:21:16 +0900

    20210307

--- php_smp/algo/22.php
+++ php_smp/algo/22.php
@@ -6,15 +6,19 @@ $arr = array();
 
 $arr[] = fscanf(STDIN, str_repeat("%d", $N));
 
-for ($i=0; $i<count($arr[0]); $i++) {
+for ($i=0; $i<$N; $i++) {
     if ($X === $arr[0][$i]) {
         unset($arr[0][$i]);
     }
 }
 
-$ans_arr = array_values($arr[0]);
-
-for ($i=0; $i<count($ans_arr); $i++) {
-    echo $ans_arr[$i] . " ";
+if (count($arr) === 0) {
+    echo PHP_EOL;
+} else{
+    $ans_arr = array_values($arr[0]);
+    for ($i=0; $i<count($ans_arr); $i++) {
+        echo $ans_arr[$i] . " ";
+    }
+    echo PHP_EOL;
 }
-echo PHP_EOL;
+
