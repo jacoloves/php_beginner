adcd02ed5c8623cca30e1aa7d87be99c4581aef8
shotaro tanaka <5511068t@gmail.com>
2021-03-09 23:08:59 +0900

    20210309

--- php_smp/algo/26.php
+++ php_smp/algo/26.php
@@ -5,5 +5,18 @@
 $arr = array();
 
 for ($i=0; $i < $N; $i++) {
-    $arr[] = fscanf(STDIN, str_replace("%d", 3));
+    $arr[] = fscanf(STDIN, str_repeat("%d", 3));
+}
+
+$min = 9999999999999;
+for ($i=0; $i<$N; $i++) {
+    if (($arr[$i][2]-$arr[$i][0] > 0) && ($min > $arr[$i][1])) {
+        $min = $arr[$i][1];
+    }
+}
+
+if ($min === 9999999999999) {
+    echo -1 . PHP_EOL;
+} else {
+    echo $min . PHP_EOL;
 }
