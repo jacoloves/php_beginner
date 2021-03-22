3da3240053601e56508efe1d204181538700bfd9
shotaro tanaka <5511068t@gmail.com>
2021-03-22 23:08:17 +0900

    20210322

--- php_smp/algo/35.php
+++ php_smp/algo/35.php
@@ -4,4 +4,16 @@
 [$C] = fscanf(STDIN, str_repeat("%d", 1));
 [$X] = fscanf(STDIN, str_repeat("%d", 1));
 
+$ans = 0;
+for ($i=0; $i<=$A; ++$i) {
+    for ($j=0; $j<=$B; ++$j) {
+        for ($k=0; $k<=$C; ++$k) {
+            $sum = 500*$i+100*$j+50*$k;
+            if ($sum == $X) {
+                $ans++;
+            }
+        }
+    }
+}
 
+echo $ans . PHP_EOL;
